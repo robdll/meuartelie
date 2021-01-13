5ccbea7217f37d616c86bc826ff9e9e5acfb1686
koop4 <dilillo.roberto@gmail.com>
2021-01-13 19:01:59 +0000

    adds nav component

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -6,12 +6,41 @@ import styles from '../../styles/Navbar.module.scss'
 /** TODO  replace hamburger with https://www.youtube.com/watch?v=dIyVTjJAkLw&ab_channel=codeSTACKr */
 /** Add navbar https://www.youtube.com/watch?v=F0uED-w9E48&ab_channel=w3newbie */
 
-const Navbar = _ => (
-  <div className={styles.navbar}>
-        <i>Meuartelie</i>
-        <NavMenuIcon />
-  </div>
-);
+
+
+class Navbar extends React.Component {
+  
+  render() {
+    let toggle = styles.closed;
+
+    const Menu = _ => (
+      <div className={styles.menu__container}>
+        <div className={styles.menu__content}>
+          <ul className={styles.menu__list}>
+            <li className={styles.menu__item}>Home</li>
+            <li className={styles.menu__item}>Services</li>
+            <li className={styles.menu__item}>Contact</li>
+          </ul>
+        </div>
+      </div>
+    );
+
+    const handleClick = () =>  {
+      toggle = toggle == styles.open ? styles.closed : styles.open;
+      console.log(toggle);
+    }
+
+    return <div className={styles.navbar}>
+      <i>Meuartelie</i>
+      <NavMenuIcon onClick={handleClick} />
+      <Menu></Menu>
+    </div>;
+  }
+
+}
+
+
+
 
 export default Navbar;
 
