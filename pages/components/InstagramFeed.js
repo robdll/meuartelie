f8ecefc43b1ae7835b086f6c615d6020462dd57b
koop4 <dilillo.roberto@gmail.com>
2021-02-09 00:42:46 +0000

    insta feed

---
+++ pages/components/InstagramFeed.js
@@ -0,0 +1,36 @@
+
+
+import React from "react";
+import styles from '../../styles/InstagramFeed.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const InstagramFeed = props => {
+    let posts = props.feed.data;
+    posts = posts.filter((i,idx) => idx < 1 )
+    posts = posts.map( (i,idx) => <div key={idx} className={styles.photo__card}>
+        <Link href={i.permalink} passHref >
+            <a>
+            <Image
+                className={styles.photo}
+                src={`${i.media_type === 'VIDEO' ? i.thumbnail_url : i.media_url}`}
+                alt={i.caption}
+                width={250}
+                height={250}
+            />
+            </a>
+        </Link>
+    </div>
+    )
+     return (
+        <div id="instagram" className={`section ${styles.instagram}`}>
+            <h2 className={`title`}>Instagram</h2>
+            <span className={`separator ${styles.separator}`}> </span>
+            <article className={styles.photo__container}>
+                {posts }
+            </article>
+        </div>
+    );
+}
+
+export default InstagramFeed;
