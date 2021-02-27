980ac5ea65a253786c381d6985df8fe7e5468981
koop4 <dilillo.roberto@gmail.com>
2021-02-27 13:03:39 +0000

    improve service overview responsiveness

--- pages/components/Services.js
+++ pages/components/Services.js
@@ -17,28 +17,28 @@ const Services = _ => (
             <article className={styles.card}>
                 <MandalaArtIcon className={styles.icon}></MandalaArtIcon>
                 <h3 className={styles.service__name}>Mandala Art</h3>
-                <div className={styles.more}> Veja Mais </div>
+                <div className={styles.more}> Encomende já sua Mandala Multidimensional </div>
             </article>
             </Link>
             <Link href="/services#courses">
             <article className={styles.card}>
                 <VideoocurseIcon className={styles.icon}></VideoocurseIcon>
                 <h3 className={styles.service__name}>Video Aula</h3>
-                <div className={styles.more}> Veja Mais </div>
+                <div className={styles.more}> Aprenda ou aperfeiçoe sua técnica de desenho de mandalas </div>
             </article>
             </Link>
             <Link href="/services#reading">
             <article className={styles.card}>
                 <ClassesIcon className={styles.icon}></ClassesIcon>
                 <h3 className={styles.service__name}>Consulta</h3>
-                <div className={styles.more}> Veja Mais </div>
+                <div className={styles.more}> Marque sua consulta terapêutica e comece já o processo de autocura </div>
             </article>
             </Link>
             <Link href="/services#workshop">
             <article className={styles.card}>
                 <SelfknoledgeIcon className={styles.icon}></SelfknoledgeIcon>
                 <h3 className={styles.service__name}>Workshop</h3>
-                <div className={styles.more}> Veja Mais </div>
+                <div className={styles.more}> Busque aprofundamento sobre as mandalas como tecnologias de cura </div>
             </article>
             </Link>
         </div>
