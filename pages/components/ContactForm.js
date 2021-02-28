ca375a92f420968269429576f91b22b8a65b3dbb
koop4 <dilillo.roberto@gmail.com>
2021-02-28 09:35:56 +0000

    link variable to form

--- pages/components/ContactForm.js
+++ pages/components/ContactForm.js
@@ -1,18 +1,29 @@
 
 
-import React from "react";
+import React, { useState } from "react";
 import styles from '../../styles/ContactForm.module.scss'
 
-const ContactForm = _ => (
-    <div id="contact" className={`section ${styles.contactform}`}>
-        <h2 className={`title`}>Contate-Me</h2>
-        <input type="text" className={styles.subscribe__input} placeholder="Seu Nome"/>
-        <input type="email" className={styles.subscribe__input} placeholder="Insira o endereço de e-mail"/>
-        <input type="text" className={styles.subscribe__input} placeholder="Sujeita"/>
-        <input type="textarea" className={styles.subscribe__input} placeholder="Seu Texto Aqui"/>
-        <div className={`btn ${styles.btn}`}>Enviar</div>
-    </div>
-);
+const ContactForm = _ => {
+    const [contactName, onNameChange] = useState('');
+    const [contactEmail, onEmailChange] = useState('');
+    const [contactSubject, onSubjectChange] = useState('');
+    const [contactMessage, onMessageChange] = useState('');
+    
+    const submitContactForm = () => {
+        console.log(contactName, contactEmail, contactSubject, contactMessage);
+    }
+
+    return (
+        <form id="contact" className={`section ${styles.contactform}`} onSubmit={submitContactForm}>
+            <h2 className={`title`}>Contate-Me</h2>
+            <input value={contactName} onChange={ e => { onNameChange(e.target.value);} } type="text" className={styles.subscribe__input} placeholder="Seu Nome" required/>
+            <input value={contactEmail} onChange={ e => { onEmailChange(e.target.value);} } type="email" className={styles.subscribe__input} placeholder="Insira o endereço de e-mail" required/>
+            <input value={contactSubject} onChange={ e => { onSubjectChange(e.target.value);} } type="text" className={styles.subscribe__input} placeholder="Sujeita" required/>
+            <input value={contactMessage} onChange={ e => { onMessageChange(e.target.value);} } type="textarea" className={styles.subscribe__input} placeholder="Seu Texto Aqui" required/>
+            <button type="submit" className={`btn ${styles.btn}`}>Enviar</button>
+        </form>
+    )
+}
 
 export default ContactForm;
 
