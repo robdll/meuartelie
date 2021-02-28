9fae1830fc414116753c1c4fd8c252b9a3c3550a
koop4 <dilillo.roberto@gmail.com>
2021-02-28 13:14:21 +0000

    adds carousel

--- pages/components/Testimonial.js
+++ pages/components/Testimonial.js
@@ -3,31 +3,72 @@
 import React from "react";
 import styles from '../../styles/Testimonial.module.scss'
 import Image from 'next/image'
-import Dots from "../../public/svg/3dots.svg";
+import Carousel from 'react-material-ui-carousel'
 
+const Testimonial = _ => {
 
-const Testimonial = _ => (
-    <div id="customers" className={`section ${styles.testimonial}`}>
-        <h2 className={`title ${styles.title}`}>Clientes Felizes</h2>
-        <span className={"separator"}> </span>
+    const items = [
+        {
+            imageUrl: "/png/testimonial_tania.webp",
+            feedback: "O meu despertar da consciência foi um processo guiado pela Cintya Flores. Desenhar mandalas e compreender sua simbologia foi determinante neste processo",
+            name: "Tânia Dantas",
+            location: "Bahia, Brasil"
+        },
+        {
+            imageUrl: "/png/testimonial_creuza.webp",
+            feedback: "Descubra seus mistérios e melhores sentimentos dentro das mágicas mandalas. Elas me levavam a esta linda viagem!",
+            name: "Creuza Conceição",
+            location: "Bahia, Brasil"
+        }
+    ]
+
+    return (
+        <div id="customers" className={`section ${styles.testimonial}`}>
+            <h2 className={`title ${styles.title}`}>Clientes Felizes</h2>
+            <span className={"separator"}> </span>
+
+            <Carousel
+                className={styles.carousel}
+                autoPlay={true}
+                stopAutoPlayOnHover={true}
+                interval={5000}
+                animation={"slide"}
+                swipe={true}
+                navButtonsAlwaysVisible={false}
+                navButtonsProps={{ 
+                    style: {
+                        backgroundColor: "#8f34eb",
+                        opacity: 0.4
+                    }
+                }} 
+            >
+                {
+                    items.map( (item, i) => <Item key={i} item={item} /> )
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+function Item(props)
+{
+    return (
         <article className={styles.testimonial__card}>
             <div className={styles.testimonial__photo_container}>
                 <Image
                     className={styles.testimonial__photo}
-                    src="/png/testimonial_tania.webp"
+                    src={props.item.imageUrl}
                     alt="Testimonial"
                     width={312}
                     height={300}
                 />
             </div>
-            <p className={styles.testimonial__copy}>O meu despertar da consciência foi um processo guiado pela Cintya Flores. Desenhar mandalas e compreender sua simbologia foi determinante neste processo</p>
-            <span className={styles.testimonial__name}>Tânia Dantas</span>
-            <span className={styles.testimonial__city}>Bahia, Brasil</span>
+            <p className={styles.testimonial__copy}>{props.item.feedback}</p>
+            <span className={styles.testimonial__name}>{props.item.name}</span>
+            <span className={styles.testimonial__city}>{props.item.location}</span>
         </article>
-        <Dots></Dots>
-
-    </div>
-);
+    )
+}
 
 export default Testimonial;
 
