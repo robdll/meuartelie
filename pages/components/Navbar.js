5f293c282be52c2662d653a49305cd8cccc3471c
koop4 <dilillo.roberto@gmail.com>
2021-03-05 20:52:12 +0000

    adds logo

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import styles from '../../styles/Navbar.module.scss';
 import Menu from './MenuOverlay';
 import Link from 'next/link';
+import Logo from "../../public/svg/Logo.svg";
 
 function Navbar() {
   const [menu, setToggle] = useState(false);
@@ -10,7 +11,7 @@ function Navbar() {
 
   return (
     <div className={styles.navbar}>
-        <span className={styles.logo}><Link href="/">Meu Arteliê</Link></span>
+        <span className={styles.logo}><Link href="/"><Logo  /></Link></span>
         <div className={`${styles.menu_btn} ${menu ? styles.open : ''}`} onClick={toggleMenu}>
           <div className={styles.menu_btn__burger}></div>
         </div>
