bf7e6bc9f0cd821289686ef31e4d4fc501812ca0
Roberto Di Lillo <roberto.dilillo@garycom.com>
2023-06-23 16:13:48 +0200

    i18n main app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import '../styles/globals.scss'
 import Head from 'next/head'
+import withI18n from "./i18n/withI18n";
+import { useTranslation } from 'react-i18next'
 
 function MyApp({ Component, pageProps }) {
+  const { t } = useTranslation();
   return <>
     <Head>
-      <title>Meu Arteliê</title>
+      <title>Meu Arteliê {t('hello')}</title>
       <meta name="viewport"  key="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" key="description" content="Descubra as mandalas e seus significados. Aprenda a desegnar, colorir e manifestar." />
     </Head>
@@ -13,4 +16,4 @@ function MyApp({ Component, pageProps }) {
   </>
 }
 
-export default MyApp
+export default withI18n(MyApp)
