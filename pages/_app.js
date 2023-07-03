7eab0db94c120c05d0b173a4dca958e6e5315809
Roberto Di Lillo <roberto.dilillo@garycom.com>
2023-07-03 16:07:31 +0200

    adds font family

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,21 @@ import '../styles/globals.scss'
 import Head from 'next/head'
 import withI18n from "./i18n/withI18n";
 import { useTranslation } from 'react-i18next'
+import localFont from 'next/font/local'
+
+const appFont = localFont({ src: './fonts/Jokerman.woff2' })
+
 
 function MyApp({ Component, pageProps }) {
   const { t } = useTranslation();
+  console.log(t('hello'));
   return <>
     <Head>
-      <title>Meu Arteliê {t('hello')}</title>
+      <title>Meu Arteliê</title>
       <meta name="viewport"  key="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" key="description" content="Descubra as mandalas e seus significados. Aprenda a desegnar, colorir e manifestar." />
     </Head>
-       
-    <Component {...pageProps} />
+    <Component {...pageProps} className={appFont.className} />
   </>
 }
 
