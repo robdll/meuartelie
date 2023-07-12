61f5d18cb913d7c46b8e750d9dbd72da6850d52f
robdll <dilillo.roberto@gmail.com>
2023-07-12 21:14:00 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-  
-      return config;
-    },
-    images: {
-      domains: ['scontent.cdninstagram.com'],
-    }
-  };
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  i18n: {
+    defaultLocale: 'pt',
+    locales: ['pt', 'en', 'it'],
+    localeDetection: false,
+  },
+  images: {
+    domains: ['scontent.cdninstagram.com'],
+  }
+};
